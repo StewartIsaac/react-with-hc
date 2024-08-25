bcf0a4a2dc358acf813f7a36e12b8673647f838e
Gabriel Isaac <stewartisaac90@gmail.com>
2024-08-25 21:01:52 +0100

    implement persistence middleware

--- react-router-ex/src/stores/useCounterStore.jsx
+++ react-router-ex/src/stores/useCounterStore.jsx
@@ -1,12 +1,18 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware'
 
 const initialState = { count: 0 };
 
-const useCounterStore = create((set) => ({
+const useCounterStore = create(persist((set) => ({
     ...initialState,
     increment: () => set((state) => ({ count: state.count + 1 })),
     decrement: () => set((state) => ({ count: state.count - 1 })),
-    resetCount: () => set((state) => ({ count: state.count = 0 })),
-}));
+    resetCount: () => set({ count: 0 })
+}),
+    {
+        name: 'counter-storage',
+        getStorage: () => localStorage,
+    }
+));
 
 export default useCounterStore;
